d3e8c9076ffd4883aa87af5ba1a862fe96a5d424
Matthias <rocketpencil.studios@gmail.com>
2020-12-27 22:53:55 +0100

    Implement the parsing of functions

--- TotemLang/src/main/java/com/github/polyrocketmatt/totem/parser/parsers/FunctionParser.java
+++ TotemLang/src/main/java/com/github/polyrocketmatt/totem/parser/parsers/FunctionParser.java
@@ -4,13 +4,11 @@ import com.github.polyrocketmatt.totem.exception.ParserException;
 import com.github.polyrocketmatt.totem.lexical.Token;
 import com.github.polyrocketmatt.totem.lexical.TokenType;
 import com.github.polyrocketmatt.totem.node.Node;
-import com.github.polyrocketmatt.totem.node.ObjectNode;
 import com.github.polyrocketmatt.totem.node.statements.FunctionStatementNode;
 import com.github.polyrocketmatt.totem.parser.AbstractParser;
 import com.github.polyrocketmatt.totem.parser.Acceptor;
 import com.github.polyrocketmatt.totem.parser.TotemParser;
 import com.github.polyrocketmatt.totem.utils.Parameter;
-import com.github.polyrocketmatt.totem.utils.TypeUtils;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
@@ -18,12 +16,14 @@ import java.util.List;
 
 /**
  * Created by PolyRocketMatt on 27/12/2020.
+ *
+ * Parser to parse function statements.
  */
 
 public class FunctionParser extends AbstractParser<FunctionStatementNode> implements Acceptor {
 
     /**
-     * Initialize a new ObjectParser.
+     * Initialize a new FunctionParser.
      *
      * @param parser the original parser
      */
@@ -53,10 +53,23 @@ public class FunctionParser extends AbstractParser<FunctionStatementNode> implem
         }
 
         getStream().skip(1);    //  Skip ")"
+        getStream().matchOrThrow(TokenType.ARROW);
 
+        token = getStream().read();
+        List<TokenType> types = new ArrayList<>();
+        while (token.getType() != TokenType.CPAREN) {
+            Token type = getStream().matchOrThrow(TokenType.BOOL, TokenType.FLOAT, TokenType.INT, TokenType.STRING);
 
-        return null;
-        //return new FunctionStatementNode(superNode, new LinkedList<>(), identifier.getValue().getValue().toString(), parameters);
+            if (getStream().read().getType() == TokenType.COMMA)
+                getStream().skip(1);
+
+            types.add(type.getType());
+            token = getStream().read();
+        }
+
+        getStream().skip(1);    //  Skip ")"
+
+        return new FunctionStatementNode(superNode, new LinkedList<>(), types, identifier.getValue().getValue().toString(), parameters);
     }
 
     @Override
